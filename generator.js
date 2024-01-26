70d3324e9fd25b5cf293471e613afe12ca9c9a05
Remi Rampin <remi@rampin.org>
2024-01-26 12:42:37 -0500

    Add options for generation

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-let words = [
+let planets = [
   'MERCURY',
   'VENUS',
   'EARTH',
@@ -8,284 +8,74 @@ let words = [
   'URANUS',
   'NEPTUNE',
 ];
-words = [
-  'ACTINOLITE',
-  'ADAMITE',
-  'AGATE',
-  'ALBITE',
-  'ALEXANDRITE',
-  'ALGODONITE',
-  'ALMANDINE',
-  'AMAZONITE',
-  'AMBER',
-  'AMBLYGONITE',
-  'AMETHYST',
-  'AMETRINE',
-  'AMMOLITE',
-  'ANALCIME',
-  'ANATASE',
-  'ANDALUSITE',
-  'ANDESINE',
-  'ANDRADITE',
-  'ANGLESITE',
-  'ANHYDRITE',
-  'ANORTHITE',
-  'APATITE',
-  'APOPHYLLITE',
-  'AQUAMARINE',
-  'ARAGONITE',
-  'AUGELITE',
-  'AXINITE',
-  'AZURITE',
-  'BARITE',
-  'BAYLDONITE',
-  'BENITOITE',
-  'BERYL',
-  'BERYLLONITE',
-  'BISMUTOTANTALITE',
-  'BLOODSTONE',
-  'BOLEITE',
-  'BORACITE',
-  'BORNITE',
-  'BRAZILIANITE',
-  'BREITHAUPTITE',
-  'BROOKITE',
-  'BRUCITE',
-  'BUSTAMITE',
-  'BYTOWNITE',
-  'CALCITE',
-  'CANASITE',
-  'CANCRINITE',
-  'CARNELIAN',
-  'CASSITERITE',
-  'CATAPLEIITE',
-  'CELESTITE',
-  'CERULEITE',
-  'CERUSSITE',
-  'CHABAZITE',
-  'CHALCEDONY',
-  'CHAMBERSITE',
-  'CHAROITE',
-  'CHILDRENITE',
-  'CHIOLITE',
-  'CHROMITE',
-  'CHRYSOBERYL',
-  'CHRYSOCOLLA',
-  'CHRYSOPRASE',
-  'CINNABAR',
-  'CITRINE',
-  'CLINOCHLORE',
-  'COBALTITE',
-  'COLEMANITE',
-  'CORAL',
-  'CORDIERITE',
-  'CORUNDUM',
-  'COVELLITE',
-  'CREEDITE',
-  'CROCOITE',
-  'CRYOLITE',
-  'CUPRITE',
-  'DANBURITE',
-  'DATOLITE',
-  'DIAMOND',
-  'DIASPORE',
-  'DICKINSONITE',
-  'DIOPSIDE',
-  'DIOPTASE',
-  'DOLOMITE',
-  'DUMORTIERITE',
-  'EKANITE',
-  'EMERALD',
-  'ENSTATITE',
-  'EOSPHORITE',
-  'EPIDOTE',
-  'ETTRINGITE',
-  'EUCLASE',
-  'EUDIALYTE',
-  'EUXENITE',
-  'FELDSPAR',
-  'FERGUSONITE',
-  'FLUORITE',
-  'FRIEDELITE',
-  'GADOLINITE',
-  'GAHNOSPINEL',
-  'GARNET',
-  'GAYLUSSITE',
-  'GRANDIDIERITE',
-  'GYPSUM',
-  'HAMBERGITE',
-  'HAUYNE',
-  'HELIODOR',
-  'HEMATITE',
-  'HEMIMORPHITE',
-  'HERDERITE',
-  'HESSONITE',
-  'HODGKINSONITE',
-  'HOLTITE',
-  'HOWLITE',
-  'HUEBNERITE',
-  'HUMITE',
-  'HUREAULITE',
-  'HURLBUTITE',
-  'HYPERITDIABAS',
-  'VESUVIANITE',
-  'INDERITE',
-  'IOLITE',
-  'JADEITE',
-  'JASPER',
-  'JEREMEJEVITE',
-  'JET',
-  'KORNERUPINE',
-  'KURNAKOVITE',
-  'KYANITE',
-  'KAMMERERITE',
-  'LABRADORITE',
-  'LANGBEINITE',
-  'LAPISLAZULI',
-  'LASERBLUE',
-  'LAWSONITE',
-  'LAZULITE',
-  'LEGRANDITE',
-  'LEPIDOLITE',
-  'LEUCITE',
-  'LINARITE',
-  'LUDLAMITE',
-  'MAGNESITE',
-  'MALACHITE',
-  'MANGANOTANTALITE',
-  'MARCASITE',
-  'MELIPHANITE',
-  'MELLITE',
-  'MICROCLINE',
-  'MICROLITE',
-  'MILARITE',
-  'MILLERITE',
-  'MIMETITE',
-  'MOLDAVITE',
-  'MONAZITE',
-  'MOONSTONE',
-  'MORDENITE',
-  'MORGANITE',
-  'NAMBULITE',
-  'NATROLITE',
-  'MESOLITE',
-  'SCOLECITE',
-  'NEPHELINE',
-  'NEPHRITE',
-  'NEPTUNITE',
-  'NICCOLITE',
-  'OBSIDIAN',
-  'OLIGOCLASE',
-  'ONYX',
-  'OPAL',
-  'ORTHOCLASE',
-  'PAINITE',
-  'PALYGORSKITE',
-  'PAPAGOITE',
-  'PARGASITE',
-  'PARISITE',
-  'PEARL',
-  'PECTOLITE',
-  'PENTLANDITE',
-  'PERICLASE',
-  'PERIDOT',
-  'PERISTERITE',
-  'PERTHITE',
-  'PETALITE',
-  'PHENAKITE',
-  'PHOSGENITE',
-  'PHOSPHOPHYLLITE',
-  'POLLUCITE',
-  'POWELLITE',
-  'PREHNITE',
-  'PROSOPITE',
-  'PROTEUS',
-  'PROUSTITE',
-  'PUMPELLYITE',
-  'PURPURITE',
-  'PYRARGYRITE',
-  'PYRITE',
-  'PYROPHYLLITE',
-  'PYROXMANGITE',
-  'PYRRHOTITE',
-  'QUARTZ',
-  'QUARTZITE',
-  'REALGAR',
-  'RHODIZITE',
-  'RHODOCHROSITE',
-  'RHODONITE',
-  'RUBELLITE',
-  'RUBY',
-  'RUTILE',
-  'SAMARSKITE',
-  'SANIDINE',
-  'SAPPHIRE',
-  'SAPPHIRINE',
-  'SARCOLITE',
-  'SCAPOLITE',
-  'SCHEELITE',
-  'SCORODITE',
-  'SELLAITE',
-  'SENARMONTITE',
-  'SERANDITE',
-  'SERPENTINE',
-  'SHATTUCKITE',
-  'SHORTITE',
-  'SIDERITE',
-  'SILLIMANITE',
-  'SIMPSONITE',
-  'SINHALITE',
-  'SMALTITE',
-  'SMITHSONITE',
-  'SODALITE',
-  'SOGDIANITE',
-  'SPHALERITE',
-  'SPINEL',
-  'SPODUMENE',
-  'SPURRITE',
-  'STAUROLITE',
-  'STIBIOTANTALITE',
-  'STICHTITE',
-  'STOLZITE',
-  'STRONTIANITE',
-  'SUGILITE',
-  'SUNSTONE',
-  'TAAFFEITE',
-  'TALC',
-  'TANTALITE',
-  'TANZANITE',
-  'TEKTITE',
-  'TEPHROITE',
-  'THAUMASITE',
-  'THOMSONITE',
-  'TOPAZ',
-  'TOURMALINE',
-  'TREMOLITE',
-  'TRIPHYLITE',
-  'TUGTUPITE',
-  'TURQUOISE',
-  'ULEXITE',
-  'VANADINITE',
-  'VARISCITE',
-  'VILLIAUMITE',
-  'VIVIANITE',
-  'VAYRYNENITE',
-  'WARDITE',
-  'WAVELLITE',
-  'WELOGANITE',
-  'WHEWELLITE',
-  'WILKEITE',
-  'WILLEMITE',
-  'WITHERITE',
-  'WOLLASTONITE',
-  'WULFENITE',
-  'XONOTLITE',
-  'YUGAWARALITE',
-  'ZEKTZERITE',
-  'ZINCITE',
-  'ZIRCON',
-  'ZUNYITE',
+let gems = [
+  'ACTINOLITE', 'ADAMITE', 'AGATE', 'ALBITE', 'ALEXANDRITE', 'ALGODONITE',
+  'ALMANDINE', 'AMAZONITE', 'AMBER', 'AMBLYGONITE', 'AMETHYST', 'AMETRINE',
+  'AMMOLITE', 'ANALCIME', 'ANATASE', 'ANDALUSITE', 'ANDESINE', 'ANDRADITE',
+  'ANGLESITE', 'ANHYDRITE', 'ANORTHITE', 'APATITE', 'APOPHYLLITE',
+  'AQUAMARINE', 'ARAGONITE', 'AUGELITE', 'AXINITE', 'AZURITE', 'BARITE',
+  'BAYLDONITE', 'BENITOITE', 'BERYL', 'BERYLLONITE', 'BISMUTOTANTALITE',
+  'BLOODSTONE', 'BOLEITE', 'BORACITE', 'BORNITE', 'BRAZILIANITE',
+  'BREITHAUPTITE', 'BROOKITE', 'BRUCITE', 'BUSTAMITE', 'BYTOWNITE', 'CALCITE',
+  'CANASITE', 'CANCRINITE', 'CARNELIAN', 'CASSITERITE', 'CATAPLEIITE',
+  'CELESTITE', 'CERULEITE', 'CERUSSITE', 'CHABAZITE', 'CHALCEDONY',
+  'CHAMBERSITE', 'CHAROITE', 'CHILDRENITE', 'CHIOLITE', 'CHROMITE',
+  'CHRYSOBERYL', 'CHRYSOCOLLA', 'CHRYSOPRASE', 'CINNABAR', 'CITRINE',
+  'CLINOCHLORE', 'COBALTITE', 'COLEMANITE', 'CORAL', 'CORDIERITE', 'CORUNDUM',
+  'COVELLITE', 'CREEDITE', 'CROCOITE', 'CRYOLITE', 'CUPRITE', 'DANBURITE',
+  'DATOLITE', 'DIAMOND', 'DIASPORE', 'DICKINSONITE', 'DIOPSIDE', 'DIOPTASE',
+  'DOLOMITE', 'DUMORTIERITE', 'EKANITE', 'EMERALD', 'ENSTATITE', 'EOSPHORITE',
+  'EPIDOTE', 'ETTRINGITE', 'EUCLASE', 'EUDIALYTE', 'EUXENITE', 'FELDSPAR',
+  'FERGUSONITE', 'FLUORITE', 'FRIEDELITE', 'GADOLINITE', 'GAHNOSPINEL',
+  'GARNET', 'GAYLUSSITE', 'GRANDIDIERITE', 'GYPSUM', 'HAMBERGITE', 'HAUYNE',
+  'HELIODOR', 'HEMATITE', 'HEMIMORPHITE', 'HERDERITE', 'HESSONITE',
+  'HODGKINSONITE', 'HOLTITE', 'HOWLITE', 'HUEBNERITE', 'HUMITE', 'HUREAULITE',
+  'HURLBUTITE', 'HYPERITDIABAS', 'VESUVIANITE', 'INDERITE', 'IOLITE',
+  'JADEITE', 'JASPER', 'JEREMEJEVITE', 'JET', 'KORNERUPINE', 'KURNAKOVITE',
+  'KYANITE', 'KAMMERERITE', 'LABRADORITE', 'LANGBEINITE', 'LAPISLAZULI',
+  'LASERBLUE', 'LAWSONITE', 'LAZULITE', 'LEGRANDITE', 'LEPIDOLITE', 'LEUCITE',
+  'LINARITE', 'LUDLAMITE', 'MAGNESITE', 'MALACHITE', 'MANGANOTANTALITE',
+  'MARCASITE', 'MELIPHANITE', 'MELLITE', 'MICROCLINE', 'MICROLITE', 'MILARITE',
+  'MILLERITE', 'MIMETITE', 'MOLDAVITE', 'MONAZITE', 'MOONSTONE', 'MORDENITE',
+  'MORGANITE', 'NAMBULITE', 'NATROLITE', 'MESOLITE', 'SCOLECITE', 'NEPHELINE',
+  'NEPHRITE', 'NEPTUNITE', 'NICCOLITE', 'OBSIDIAN', 'OLIGOCLASE', 'ONYX',
+  'OPAL', 'ORTHOCLASE', 'PAINITE', 'PALYGORSKITE', 'PAPAGOITE', 'PARGASITE',
+  'PARISITE', 'PEARL', 'PECTOLITE', 'PENTLANDITE', 'PERICLASE', 'PERIDOT',
+  'PERISTERITE', 'PERTHITE', 'PETALITE', 'PHENAKITE', 'PHOSGENITE',
+  'PHOSPHOPHYLLITE', 'POLLUCITE', 'POWELLITE', 'PREHNITE', 'PROSOPITE',
+  'PROTEUS', 'PROUSTITE', 'PUMPELLYITE', 'PURPURITE', 'PYRARGYRITE', 'PYRITE',
+  'PYROPHYLLITE', 'PYROXMANGITE', 'PYRRHOTITE', 'QUARTZ', 'QUARTZITE',
+  'REALGAR', 'RHODIZITE', 'RHODOCHROSITE', 'RHODONITE', 'RUBELLITE', 'RUBY',
+  'RUTILE', 'SAMARSKITE', 'SANIDINE', 'SAPPHIRE', 'SAPPHIRINE', 'SARCOLITE',
+  'SCAPOLITE', 'SCHEELITE', 'SCORODITE', 'SELLAITE', 'SENARMONTITE',
+  'SERANDITE', 'SERPENTINE', 'SHATTUCKITE', 'SHORTITE', 'SIDERITE',
+  'SILLIMANITE', 'SIMPSONITE', 'SINHALITE', 'SMALTITE', 'SMITHSONITE',
+  'SODALITE', 'SOGDIANITE', 'SPHALERITE', 'SPINEL', 'SPODUMENE', 'SPURRITE',
+  'STAUROLITE', 'STIBIOTANTALITE', 'STICHTITE', 'STOLZITE', 'STRONTIANITE',
+  'SUGILITE', 'SUNSTONE', 'TAAFFEITE', 'TALC', 'TANTALITE', 'TANZANITE',
+  'TEKTITE', 'TEPHROITE', 'THAUMASITE', 'THOMSONITE', 'TOPAZ', 'TOURMALINE',
+  'TREMOLITE', 'TRIPHYLITE', 'TUGTUPITE', 'TURQUOISE', 'ULEXITE', 'VANADINITE',
+  'VARISCITE', 'VILLIAUMITE', 'VIVIANITE', 'VAYRYNENITE', 'WARDITE',
+  'WAVELLITE', 'WELOGANITE', 'WHEWELLITE', 'WILKEITE', 'WILLEMITE',
+  'WITHERITE', 'WOLLASTONITE', 'WULFENITE', 'XONOTLITE', 'YUGAWARALITE',
+  'ZEKTZERITE', 'ZINCITE', 'ZIRCON', 'ZUNYITE',
 ];
+
+let options = document.getElementById('wordsearch-options');
+
+function loadWords(words) {
+  options.elements.words.value = words.join('\n');
+}
+
+document.getElementById('load-planets').addEventListener('click', function(e) {
+  e.preventDefault();
+  loadWords(planets);
+});
+
+document.getElementById('load-gems').addEventListener('click', function(e) {
+  e.preventDefault();
+  loadWords(gems);
+});
 
 
 let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -302,11 +92,37 @@ let directions = [
   [-1, -1],
 ];
 
-window.addEventListener('load', function () {
-  generateGrid(words, 35, 30);
+options.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  let width = options.elements.width.value;
+  let height = options.elements.height.value;
+  let backwards = options.elements.backwards.checked;
+
+  let words = options.elements.words.value;
+  words = words.split(/\s+/);
+  let filteredWords = [];
+  for(let i = 0; i < words.length; ++i) {
+    let word = words[i].toUpperCase();;
+    if(word.length === 0) {
+      continue;
+    }
+    for(let c = 0; c < word.length; ++c) {
+      if(letters.indexOf(word[c]) === -1) {
+        alert('Invalid word: ' + word);
+      }
+    }
+    filteredWords.push(word);
+  }
+  if(filteredWords.length === 0) {
+    return;
+  }
+  options.elements.words.value = filteredWords.join('\n');
+
+  generateGrid(filteredWords, width, height, backwards);
 });
 
-function generateGrid(words, width, height) {
+function generateGrid(words, width, height, backwards) {
   // Sort word list by decreasing length
   words = words.slice();
   words.sort(function(a, b) {
@@ -415,6 +231,7 @@ function generateGrid(words, width, height) {
 
   // Fill the grid element in the page with the letters
   let gridElem = document.getElementById('wordsearch-grid');
+  gridElem.innerHTML = '';
   let gridSet = 0;
   for(let y = 0; y < height; ++y) {
     let rowElem = document.createElement('tr');
@@ -432,6 +249,7 @@ function generateGrid(words, width, height) {
 
   // List the words to be found
   let listElem = document.getElementById('wordsearch-list');
+  listElem.innerHTML = '';
   let listedWords = [];
   let placedCount = 0;
   for(let i = 0; i < words.length; ++i) {
